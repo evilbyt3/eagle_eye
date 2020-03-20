85a55a21cb4741fe643040f7235b6f0f5c06c892
JellyPi101 <JellyPi101@protonmail.com>
2020-03-20 12:40:44 +0200

    Added colors and emplemented function to display a basic overview of the host OS

--- stat.sh
+++ stat.sh
@@ -23,8 +23,59 @@ function usage() {
 	printf "  Usage: $script_name <options>\n\t-i -- Show system information\n\t-u -- Show usage (cpu, hard disk partitions)\n\t-s -- Show status of the provided services. Services need to be separated by a space (default: nginx, tor, sshd, mongodb)\n\t-t -- Show temperatures (CPU Cores, Partitions)\n\t-p -- Show processes\n"
 }
 
+# This function displays a basic overview of your system
+# such as: Distribution, kernel version, processes, memory, etc
+function display_system_info() {
+	# Distro
+	distro=$(cat /etc/*release | grep PRETTY_NAME | cut -d "=" -f 2- | sed 's/"//g')
+
+	# Kernel
+	kernel=$(uname -sr)
+
+	# Uptime
+	uptime=$(uptime -p)
+
+	# Load
+	load=`cat /proc/loadavg | awk '{ print $1,$2,$3,$5 }'`
+
+	# Processes
+	procs=`ps -eo user | sort | uniq -c | awk '{print $2 " " $1}'`
+	proc_root=`echo "$procs" | grep root | awk '{print $2}'`
+	proc_user=`echo "$procs" | grep -v root | awk '{print $2}' | awk '{ SUM += $1} END { print SUM }'`
+	proc_all=`echo "$procs" | awk '{print $2}' | awk '{ SUM += $1} END { print SUM }'`
+
+	# CPU
+	CPU=`grep "model name" /proc/cpuinfo | cut -d ' ' -f3- | head -1`
+
+	# Memory
+	IFS=" " read total used free <<<$(free -htm | grep "Mem" | awk {'print $3,$4,$2'})
+
+	echo -e "${BOLD}${YELLOW}system info:${RST}\n
+  Distro....:	$distro
+  Uptime....:	$uptime
+  Kernel....:	$kernel
+
+  Load......:	$load
+  Processes.:	${GREEN}$proc_all${RST} (total)	${GREEN}$proc_root${RST} (root)	${GREEN}$proc_user${RST} (user)
+
+  Memory....:	${GREEN}$total${RST} (total)	${GREEN}$used${RST} (used)	${GREEN}$free${RST} (free)
+  CPU.......:	$CPU\n"
+}
+
+
 
 #### GLOBAL VARIABLES ####
+
+# Colors
+RST=`tput sgr0`
+RED=`tput setaf 1`
+BOLD=`tput bold`
+GREEN=`tput setaf 2`
+BLACK=`tput setaf 0`
+WHITE=`tput setaf 7`
+YELLOW=`tput setaf 3`
+dim="\e[2m"
+undim="\e[0m"
 
 # Bools
 sprocs=0
@@ -66,5 +117,5 @@ while getopts ":pnuis:h" opts; do
 	no_args=0
 done
 
-# [ $sinfo -eq 1 ] && display_system_info
+[ $sinfo -eq 1 ] && display_system_info
 # [ -n $sprocs ] && 
